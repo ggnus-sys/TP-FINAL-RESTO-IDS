40e671e54c4aaf54f999baa4a5ed883941e3104d
GoGonus <ggnus@fi.uba.ar>
2026-05-25 16:26:33 -0300

    Dockerizado implementado
    La api y la base de datos correran en contenedores separados, y cuando se empieze a desarrollar el front este tambien correra sobre otro mas. El contenedor de la db inicializara la db y el de la api esperara hasta que esta se cree, y se activara.

---
+++ db/init_db.sql
@@ -0,0 +1,42 @@
+USE kaifer_db;
+
+CREATE TABLE menu(
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    plato VARCHAR(100) NOT NULL,
+    precio INT NOT NULL,
+    descripcion VARCHAR(150) NOT NULL,
+    restricciones_alimenticias SET ('vegetariano', 'vegano', 'sin_lactosa', 'sin_gluten') NOT NULL  
+);
+
+
+CREATE TABLE usuarios (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    contrasena VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    rol ENUM('admin', 'cliente') NOT NULL DEFAULT 'cliente'
+);
+
+
+CREATE TABLE resenas(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    contenido VARCHAR(200) NOT NULL,
+    estrellas INT CHECK (estrellas BETWEEN 1 AND 5) NOT NULL,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_usuario INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE reservas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente INT NOT NULL,   
+    mesas INT NOT NULL CHECK (mesas BETWEEN 1 AND 30), -- maximo supuesto de 30 por el moemnto
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_reserva DATE NOT NULL,
+    estado_reserva ENUM('pendiente', 'confirmada', 'cancelada') NOT NULL DEFAULT 'pendiente',
+    FOREIGN KEY (cliente) REFERENCES usuarios(id) ON DELETE CASCADE
+);
